2ee2cdcd8817defa14171e1d9bda25a67ad9d599
Diego Giovanazzi <diegogiovanazzi@gmail.com>
2015-01-25 23:35:39 -0300

    Clase Principal y Frame

---
+++ src/Ventanas/FramePrincipal.java
@@ -0,0 +1,23 @@
+package Ventanas;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class FramePrincipal extends JFrame {
+
+	private JPanel contentPane;
+
+	
+	public FramePrincipal() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
